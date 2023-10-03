3981c6fea91acada5d3e334646621dcbbf3935ab
Hakan Yildiz <yildizhakan88@gmail.com>
2023-10-03 12:18:43 +0000

    gitpod config and tcp listener code.

---
+++ TCP Server/tcp.cs
@@ -0,0 +1,40 @@
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace TCPConsole{
+
+    class Program{
+
+        static void Main(string[] args){
+
+            TcpListener listener = new TcpListener(IPAddress.Any,5000);
+            listener.Start();
+
+            var client = listener.AcceptTcpClient();
+            NetworkStream networkStream = client.GetStream();
+            StreamWriter writer = new StreamWriter(networkStream);
+            writer.AutoFlush = true;
+
+            while(true){
+                byte [] buffer = new byte[client.ReceiveBufferSize];
+                int bytesRead = networkStream.Read(buffer, 0, client.ReceiveBufferSize);
+
+                String datareceived = Encoding.ASCII.GetString(buffer, 0, bytesRead);
+                if(bytesRead != 0){
+                    Console.WriteLine("Received Request: \n" + datareceived);
+                    string response = "Works";
+                    Console.WriteLine("response is \n" + response);
+                    writer.WriteLine(response);
+                } else {
+                    break;
+                }
+
+            }
+            Console.ReadKey();
+
+
+        }
+    }
+}
---
+++ notes.cs
@@ -0,0 +1,6 @@
+/* TCP:
+
+Single connection to send and receive,
+Trust
+
+*/
